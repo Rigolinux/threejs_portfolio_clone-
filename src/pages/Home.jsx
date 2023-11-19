99876786baf48b3a769e7b38c49e2a182876fea0
rigolinux <rigoberto7567@gmail.com>
2023-11-19 10:48:37 -0600

    island created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,49 @@
 
-
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import Loader from "../components/Loader"
+import Island from "../models/Island"
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [screenScale, screenPosition] = adjustIslandForScreenSize()
+
   return (
-    <div>Home</div>
+    <section className="w-full h-screen relative">
+     {/*  <div className="absolute top-28 left-0 right-0  z-10 flex
+        items-center justify-center">
+      
+
+      </div> */}
+      <Canvas className="w-full h-screen bg-transparent"
+        camera={ {near: 0.1, far: 1000, }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight  />
+          <ambientLight  />
+          <pointLight  />
+          <spotLight  />
+          <hemisphereLight  />
+          <Island 
+            scale={screenScale}
+            position={screenPosition}
+            rotation={[1, 4.5, 0]}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
